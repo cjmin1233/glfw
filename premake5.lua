88f492e15a4a481d156918a5663e80003d3855c1
cjmin1233 <79832540+cjmin1233@users.noreply.github.com>
2025-10-18 14:22:10 +0900

    Refactor GLFW project configuration and files
    Updated GLFW project configuration to include additional source files and headers for better platform support.

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "on"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -8,32 +9,46 @@ project "GLFW"
     files
     {
         "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
-        "src/context.c",
-        "src/init.c",
-        "src/input.c",
-        "src/monitor.c",
-        "src/vulkan.c",
-        "src/window.c"
+		"include/GLFW/glfw3native.h",
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+		"src/platform.c",
+		"src/vulkan.c",
+		"src/window.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c"
     }
 
     filter "system:windows"
-        buildoptions {"-std=c11", "-lgdi32"}
-        staticruntime "On"
+        buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
 
         files
         {
             "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
+			"src/win32_module.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.h",
+			"src/win32_time.c",
+			"src/win32_thread.h",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
         }
 
         defines
@@ -42,16 +57,18 @@ project "GLFW"
             "_CRT_SECURE_NO_WARNINGS"
         }
 
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
+
     filter "configurations:Debug"
         runtime "Debug"
         symbols "on"
 
     filter "configurations:Release"
         runtime "Release"
-        optimize "speed"
+        optimize "on"
 
     filter "configurations:Dist"
         runtime "Release"
         optimize "speed"
-
         symbols "off"
